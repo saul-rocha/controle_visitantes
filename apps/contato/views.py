080a8f1d5349cc4f01e8530c20a3a0f014ee21cc
Saul Rocha <55061070+saul-rocha@users.noreply.github.com>
2022-04-27 14:30:52 -0300

    contato e ajuda implementados

--- apps/contato/views.py
+++ apps/contato/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from .forms import ContatoForm
+from django.shortcuts import (
+    render, redirect, get_object_or_404
+)
+from django.contrib import messages
 
 def contato_view(request):
-    return render(request, 'Fale_Conosco.html')
+    if request.method == "POST":
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            messages.success(
+                request,
+                "Sua mensagem foi enviada, obrigado!"
+            )
+
+            return redirect("index")
+
+    form = ContatoForm()
+    context ={'form': form}
+    return render(request, 'Fale_Conosco.html', context)
